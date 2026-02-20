3fb5bfe5cceb0f670ea89c4bf87eb82d0d6ce898
Manasghosh7681 <manasghosh923@gmail.com>
2026-02-20 19:37:25 +0530

    Cors fix

--- index.js
+++ index.js
@@ -3,7 +3,10 @@ const app=express()
 const cors=require('cors')
 //middleware
 app.use(express.json())  //for json body
-app.use(cors())  //for cors error
+app.use(cors({
+  origin: "https://todo-frontend-8l6n.onrender.com",
+  credentials: true
+})); //for cors error
 
 //export
 module.exports=app
