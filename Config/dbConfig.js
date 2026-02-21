f1d3955f6672f1f2d5e379a89a7198505b977e72
Manasghosh7681 <manasghosh923@gmail.com>
2026-02-21 20:34:18 +0530

    Bug fix

--- Config/dbConfig.js
+++ Config/dbConfig.js
@@ -2,8 +2,8 @@ const mongo=require('mongoose')
 require('dotenv').config()
 const dbConnection=async()=>{
     try{
-        // await mongo.connect(process.env.MONGO_URL)
-        await mongo.connect('mongodb://localhost:27017/Manas')
+        await mongo.connect(process.env.MONGO_URL)
+        // await mongo.connect('mongodb://localhost:27017/Manas')
         console.log('Database connected Succesfully')
     }catch(err){
         console.log(err.message)
